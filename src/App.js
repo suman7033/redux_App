f642062bc5f1dc0e401aeee8305a659e885269c1
suman <sumobarnwal@gmail.com>
2023-11-14 22:25:31 +0530

    Increment/Decrement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { UseSelector,useDispatch, useSelector } from 'react-redux'
+import { incNumber,decNumber } from './Action/action'
 
-function App() {
+const App = () => {
+  const myState=useSelector((state)=> state.changeNumber)
+  const dispatch =useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ReduxDemo</h1>
+      <h1>Increment/Decrement counter</h1>
+      <h4>using React and Redux</h4>
+
+      <div>
+        <button title='Decrement' onClick={()=> dispatch(decNumber())}>-</button>
+        <input name='quantity' value={myState}/>
+        <button title='Increment' onClick={()=> dispatch(incNumber())}>+</button>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
